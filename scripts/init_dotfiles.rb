dd9c4826874e4fd4fdf7e16d44078ec9fefa37dc
Axel Yoshimoto <axel@yoshimoto.me>
2017-04-26 14:37:03 +0200

    attempt to set up a fresh mac os installation (@wip)

---
+++ scripts/init_dotfiles.rb
@@ -0,0 +1,52 @@
+# other ways of executing shell commands:
+# `echo hi`
+# %x(echo hi)
+# but those do not return true ...
+
+script_dir   = File.expand_path(File.dirname(__FILE__))
+terminal_dir = script_dir.split("/")[0..-2].push("terminal").join("/")
+vim_dir      = script_dir.split("/")[0..-2].push("vim").join("/")
+git_dir      = script_dir.split("/")[0..-2].push("git").join("/")
+
+unless system 'brew help'
+  puts "-----------------------"
+  unless system 'curl -V'
+    puts "-----------------------"
+    puts "At least, provide curl"
+    exit -1
+  else
+    puts "installing homebrew"
+    system `/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`
+    puts "-----------------------"
+  end
+else
+  puts "You're already ready to brew"
+  puts "-----------------------"
+end
+
+puts "start brewing"
+puts "-----------------------"
+
+system "brew install wget git the_silver_searcher ctags yarn redis tmux htop node pgcli readline macvim"
+puts "-----------------------"
+
+puts "installing oh-my-zsh"
+system 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+puts "-----------------------"
+
+puts "removing pre-installed files and linking own"
+system 'rm ~/.zshrc ~/.gitconfig ~/.vimrc'
+system 'rm -rf ~/.vim'
+system "ln -s #{git_dir}/gitconfig ~/.gitconfig"
+system "ln -s #{git_dir}/gitignore ~/.gitignore"
+system "ln -s #{terminal_dir}/zshrc ~/.zshrc"
+system "ln -s #{vim_dir}/vimrc ~/.vimrc"
+system "ln -s #{vim_dir} ~/.vim"
+system "ln -s #{script_dir} ~/scripts"
+puts "-----------------------"
+
+
+puts "-----------------------"
+puts "-----------------------"
+puts "-----------------------"
+puts "-----------------------"
